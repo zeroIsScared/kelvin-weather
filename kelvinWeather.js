bce42fa2b464f707093b90206efe4eb269f5696c
zeroIsScared <bujornathalia@gmail.com>
2021-06-23 12:14:42 +0300

    Commiting the kelvin weather project

---
+++ kelvinWeather.js
@@ -0,0 +1,11 @@
+//The forecast today is 293 Kelvin
+const kelvin = 293;
+//Celsius is similar to Kelvin but 273 degreed less
+let celsius = kelvin-273;
+//Converting Celsius to Fahrenheit and Rounding the results
+let fahrenheit = Math.floor(celsius*(9/5)+32);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+document.write(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+

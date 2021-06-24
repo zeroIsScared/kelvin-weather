95358430b7b7608c8cdc50d5bc34108ddc4481ba
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2021-06-24 08:58:36 +0300

    Update kelvinWeather.js

--- kelvinWeather.js
+++ kelvinWeather.js
@@ -2,7 +2,7 @@
 const kelvin = 293;
 //Celsius is similar to Kelvin but 273 degreed less
 let celsius = kelvin-273;
-//Converting Celsius to Fahrenheit and Rounding the results
+//Converting Celsius to Fahrenheit and rounding the results
 let fahrenheit = Math.floor(celsius*(9/5)+32);
 
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
